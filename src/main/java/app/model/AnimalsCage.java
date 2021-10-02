7afb2985a59f6fed9e597f48e8a198c6b472f8e9
Hilkevich <pegas190685@gmail.com>
2021-10-02 20:19:27 +0300

    add @Qualifier("dog") and @Autowired.

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,29 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")             //добавил
     private Animal animal;
+
+    @Autowired                    //добавил
+    private Timer timer;          //добавил
+
+    public Timer getTimer() {     //добавил
+        return timer;             //добавил
+    }
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());                //исправил new Timer на имя бина (timer),для которого и вызывается метод getTime!Подстава!
         System.out.println("________________________");
     }
 }
